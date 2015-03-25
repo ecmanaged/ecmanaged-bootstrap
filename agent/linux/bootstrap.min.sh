05dfd6026806e7b18777f599aa49c8440d512cc0
jpdesola <jpdesola@ackstorm.es>
2015-03-26 00:46:37 +0100

    Update bootstrap.min.sh
    Added export for noninteractive mode using apt.

--- agent/linux/bootstrap.min.sh
+++ agent/linux/bootstrap.min.sh
@@ -2,6 +2,7 @@
 
 set +e
 
+export DEBIAN_FRONTEND=noninteractive
 apt-get --force-yes --yes install wget 2>/dev/null
 yum --nogpgcheck -y install wget 2>/dev/null
 wget -qO/tmp/runurl run.alestic.com/runurl
